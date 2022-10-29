fb83edcc4676850ae349042f89e64a97e22ccc8a
Rizzi <carlosadmsnt@hotmail.com>
2022-10-29 03:45:20 -0300

    Falta realizar os exercicios.

---
+++ Trabalhando-Com-Collections-Java/src/list/ExercicioListDois.java
@@ -0,0 +1,19 @@
+package list;
+
+/*
+ * Utilizando Listas, Faça uma programa que faça 5 perguntas para uma pessoa um crime. As perguntas são:
+ * 1. "Telefonou para a vítima?"
+ * 2. "Esteve no local do crime?"
+ * 3. "Mora perto da vítima?"
+ * 4. "Devia para a vítima?"
+ * 5. "Já trabalhou com a vítima?"
+ * 
+ * Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita",
+ * entre 3 e 4 como "Cúmplice" e 5 como "Assassina". Caso contrário, ela será classificada como "Inocente".
+ * https://github.com/cami-la/curso-dio-intro-collections
+ */
+
+public class ExercicioListDois
+{
+    
+}
---
+++ Trabalhando-Com-Collections-Java/src/list/ExercicioListUm.java
@@ -0,0 +1,14 @@
+package list;
+
+/*
+ * Faça um programa que receba a temperatura média dos 6 primeiros meses do ano
+ * e armazene-as em uma lista.
+ * Após isso, calcule a média semestral das temperaturas e mostre todas as temperatudas acima desta média,
+ * e em que mês ela ocorreram (Mostrar o mês por extenso: 1 - Janeiro, 2 - Fevereiro e etc).
+ * https://github.com/cami-la/curso-dio-intro-collections
+ */
+
+public class ExercicioListUm 
+{
+    
+}
